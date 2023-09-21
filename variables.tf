c319794bcba0886f4881fdfc1b9cfcad359f0b44
Victor Biga <victor.biga@gmail.com>
2023-09-21 07:18:36 +0200

    chore: clean up provider config

--- variables.tf
+++ variables.tf
@@ -4,12 +4,10 @@
 
 # Define Terraform provider
 terraform {
-  required_version = "~> 1.0"
 
   required_providers {
     google = {
       source = "hashicorp/google"
-      // version = "4.11.0" # pinning version
     }
   }
 }
